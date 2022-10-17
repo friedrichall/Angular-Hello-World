bae04b6ecc97483d9dfb86e7f3a63fe0868236c8
friedrichall <84865113+friedrichall@users.noreply.github.com>
2022-10-17 13:47:13 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HelloWorldComponent } from '../hello-world/hello-world.component';
+import { FormComponent } from '../form/form.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HelloWorldComponent ],
+  declarations: [ AppComponent, HelloComponent, HelloWorldComponent, FormComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
